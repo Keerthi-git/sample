641824aee9b22116c78e1549d5604e42c4325444
Niranjan Shrishail Hampannavar <M1023294@mindtree.com>
2018-11-15 12:54:46 +0530

    commit

---
+++ src/com/springpeople/EncryptDecrypt.java
@@ -0,0 +1,10 @@
+package com.springpeople;
+
+public class EncryptDecrypt {
+	public static String Encrypt(String password){
+		return password+"*";
+	}
+	public static String Decrypt(String password){
+		return password.substring(0, password.length()-1);
+	}
+}
